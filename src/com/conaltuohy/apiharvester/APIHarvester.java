4228dc165b2cc707e93c2c69d8a5bd88869f81e8
Conal Tuohy <conal.tuohy@gmail.com>
2016-12-30 00:12:23 +1000

    usage

--- src/com/conaltuohy/apiharvester/APIHarvester.java
+++ src/com/conaltuohy/apiharvester/APIHarvester.java
@@ -30,11 +30,42 @@ public class APIHarvester {
 			APIHarvester h = new APIHarvester(args);
 			h.run();
 		} catch (Exception e) {
-			System.err.println("Harvest failed");
+			System.err.println("Harvest failed:");
 			System.err.println(e.getMessage());
+			System.out.println();
+			printUsage();
 		}
 	}
 	
+	private static void printUsage() {
+		System.out.println("APIHarvester is a tool to harvest XML records from a web API. APIHarvester will:");
+		System.out.println();
+		System.out.println(" • download a response from a given URL, if necessary retrying in the event of failure");
+		System.out.println(" • split the response into multiple records which match an XPath expression");
+		System.out.println(" • save each record under a filename specified using another XPath expression");
+		System.out.println(" • continue harvesting from additional URLs, extracted from the response using another XPath expression");
+		System.out.println();
+		System.out.println("APIHarvester is controlled using XPath 1 expressions; see https://www.w3.org/TR/xpath/ for details.");
+		System.out.println();
+		System.out.println("Usage:");
+		System.out.println();
+		System.out.println("java -jar apiharvester.jar [parameter list]");
+		System.out.println();
+		System.out.println("Parameters are specified as [key=value]. Values containing spaces should be enclosed in quotes.");
+		System.out.println();
+		System.out.println("directory (location of output files - default is current directory)");
+		System.out.println("url (initial URL to harvest from - required)");
+		System.out.println("records-xpath (xpath identifying the individual records within a response - default is to save entire response as a single record)");
+		System.out.println("id-xpath (xpath of unique id for each record, evaluated within the context of each record - required)");
+		System.out.println("resumption-xpath (xpath of URL or URLs for subsequent pages of data - default is to harvest only from the initial URL)");
+		System.out.println("url-suffix (specifies a common suffix for URLs; useful for specifying an 'API key')");
+		System.out.println("retries (specifies a number of times to retry, in the event of any error; default is 3)");
+		System.out.println();
+		System.out.println("Example:");
+		System.out.println();
+		System.out.println("java -jar apiharvester.jar retries=4 url=\"http://example.com/api?foo=bar\" records-xpath=\"/response/result\" id-xpath=\"concat('record-', @id)\" resumption-xpath=\"concat('/api?foo=bar&page=', /response/@page-number + 1)\" url-suffix=\"&api_key=asdkfjasd\"");
+	}
+
 	private HashMap<String, String> arguments;
 	
 	private APIHarvester(String[] args) {
@@ -55,17 +86,6 @@ public class APIHarvester {
 			}
 			arguments.put(key, value);
 		}
-		System.out.println(arguments);
-		
-		// check required parameters are present
-		checkArgument("url", "The 'url' argument is required");
-		checkArgument("id-xpath", "The 'id-xpath' argument is required");
-		
-		// create output directory if needed
-		String outputDirectory = arguments.get("directory");
-		if (outputDirectory != null) {
-			new File(outputDirectory).mkdirs();
-		}
 	}
 	
 	private void checkArgument(String name, String message) {
@@ -77,12 +97,26 @@ public class APIHarvester {
 	private Stack<URL> urls = new Stack<URL>();
 	
 	private void run() throws Exception {
-		URL url = new URL(arguments.get("url") + getArgument("url-suffix", ""));
-		urls.push(url);
-		do {
-			url = urls.pop();
-			harvest(url);
-		} while (! urls.isEmpty());
+		if (arguments.isEmpty()) {
+			printUsage();
+		} else {
+			// check required parameters are present
+			checkArgument("url", "The 'url' argument is required");
+			checkArgument("id-xpath", "The 'id-xpath' argument is required");
+			
+			// create output directory if needed
+			String outputDirectory = arguments.get("directory");
+			if (outputDirectory != null) {
+				new File(outputDirectory).mkdirs();
+			}
+			
+			URL url = new URL(arguments.get("url") + getArgument("url-suffix", ""));
+			urls.push(url);
+			do {
+				url = urls.pop();
+				harvest(url);
+			} while (! urls.isEmpty());
+		}
 	}
 	
 	private int getRetries() {
@@ -90,7 +124,7 @@ public class APIHarvester {
 	}
 	
 	private void harvest(URL url) throws Exception {
-		// harvests from a url, returns the url to continue harvesting, or null if harvest complete
+		// harvests from a URL, queues up additional URLs to harvest from
 		System.out.println("harvesting from " + url + " ...");
 		// load the XML document
 		Document doc = load(url);
@@ -195,6 +229,7 @@ public class APIHarvester {
 		} catch (Exception e) {
 			System.out.println("Error reading XML. " + String.valueOf(retriesRemaining) + " retries remaining.");
 			if (retriesRemaining > 0) {
+				Thread.sleep(5000); // wait 5s before retrying
 				return load(url, retriesRemaining - 1);
 			} else {
 				throw e;
